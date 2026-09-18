76c35b9f07361ab7ea3b413fc958abbb686a5f39
Gabriel <gabriellizcfnpbnu@gmail.com>
2026-09-18 10:44:09 -0300

    feat: novas funcionalidades da pagina "setupTeams" - adicionar equipes - remover equipes - adicionar jogador - removar jogador

--- src/pages/SetupTeams/index.jsx
+++ src/pages/SetupTeams/index.jsx
@@ -1,8 +1,16 @@
 import { useGame } from "../../context/useGame.js";
+import { Trash } from "lucide-react";
 import "./SetupTeams.css";
 
 const SetupTeams = () => {
-    const { addTeam } = useGame();
+    const {
+        addTeam,
+        teams,
+        addPlayer,
+        updatePlayer,
+        removePlayer,
+        removeTeam,
+    } = useGame();
 
     return (
         <>
@@ -15,9 +23,68 @@ const SetupTeams = () => {
                 <section className="setup-section">
                     <h2>Equipes</h2>
 
-                    <button className="setup-btnAction" onClick={addTeam}>
-                        + Adicionar Equipe
-                    </button>
+                    <div className="setup-containerTeams">
+                        <button className="setup-btnAction" onClick={addTeam}>
+                            + Adicionar Equipe
+                        </button>
+
+                        {teams &&
+                            teams.map((team, teamIndex) => (
+                                <div className="setup-cardTeam">
+                                    <div className="setup-cardTem-info">
+                                        <span>Time {teamIndex + 1}</span>
+
+                                        <div className="setup-cardItem-info-acts">
+                                            <button
+                                                className="setup-btnAction btnAddPlayer"
+                                                onClick={() =>
+                                                    addPlayer(team.id)
+                                                }
+                                            >
+                                                + Jogador
+                                            </button>
+                                            <button
+                                                className="setup-btnAction"
+                                                onClick={() => removeTeam(team.id)}
+                                            >
+                                                <Trash />
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                    {team.players.map((player) => (
+                                        <>
+                                            <div className="setup-cardTeam-player">
+                                                <input
+                                                    className="input-player"
+                                                    type="text"
+                                                    value={player.name}
+                                                    placeholder="Nome do Jogador"
+                                                    onChange={(e) =>
+                                                        updatePlayer(
+                                                            team.id,
+                                                            player.id,
+                                                            e.target.value,
+                                                        )
+                                                    }
+                                                />
+                                                <button
+                                                    className="setup-btnAction"
+                                                    onClick={() =>
+                                                        removePlayer(
+                                                            team.id,
+                                                            player.id,
+                                                        )
+                                                    }
+                                                >
+                                                    <Trash />
+                                                </button>
+                                            </div>
+                                        </>
+                                    ))}
+                                </div>
+                            ))}
+                    </div>
                 </section>
             </div>
         </>
